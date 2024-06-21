67de96b0ce6fef6fc12a35f7a9439dbfc5f247e7
x <x@wechall.net>
2024-06-21 19:34:55 +0200

    Wana Mashia adala3 amla el2olal

---
+++ removePDFpassword.py
@@ -0,0 +1,25 @@
+######Best PDF password remover EVER!!!!!!!######
+######path the output  path and the password#####
+
+# Importing the PDF read AND write libraries
+from PyPDF2 import PdfReader, PdfWriter
+
+# Defining a function to reomve the password from#the PDF taking 3 parameters
+def removePDFpassword(input, output, passwd):
+    reader = PdfReader(input)
+    reader.decrypt(passwd)
+
+    writer = PdfWriter()
+
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        writer.add_page(page)
+
+    with open(output, 'wb') as HalabesaElbesa:
+        writer.write(HalabesaElbesa)
+    print(f"Password removed. New file saved as {outputPdfPath}")
+
+inputPdfPath = 'C:\\Users\\Assem Mahmoud\\Desktop\\removePDFpassword\\08099600195713508954227775638.pdf'
+outputPdfPath = 'C:\\Users\\Assem Mahmoud\\Desktop\\removePDFpassword\\ba7rMesa3lekyaHaram.pdf'
+password = '1985*0212#'
+removePDFpassword(inputPdfPath, outputPdfPath, password)
